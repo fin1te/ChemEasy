fb40a33f2f8e730f23ba31ff4cdd1094ced4993a
fin1te <fin1te@fin1te.github.com>
2021-02-03 12:58:04 +0530

    Changed MemberHome Buttons as TextView and set OnClickListener with Toast

--- app/src/main/java/com/finite/chemeasy/MainActivity.java
+++ app/src/main/java/com/finite/chemeasy/MainActivity.java
@@ -2,13 +2,28 @@ package com.finite.chemeasy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    Timer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MemberHome.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
     }
 }
--- app/src/main/java/com/finite/chemeasy/MemberHome.java
+++ app/src/main/java/com/finite/chemeasy/MemberHome.java
@@ -3,12 +3,78 @@ package com.finite.chemeasy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MemberHome extends AppCompatActivity {
+
+    private TextView mh_tv_learn,mh_tv_play,mh_tv_leaderboard,mh_tv_contact_us,mh_tv_about_us;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_home);
+        init();
+        mh_tv_learn();
+        mh_tv_play();
+        mh_tv_leaderboard();
+        mh_tv_contact_us();
+        mh_tv_about_us();
     }
+
+    private void init() {
+        mh_tv_learn = findViewById(R.id.mh_tv_learn);
+        mh_tv_play = findViewById(R.id.mh_tv_play);
+        mh_tv_leaderboard = findViewById(R.id.mh_tv_leaderboard);
+        mh_tv_contact_us = findViewById(R.id.mh_tv_contact_us);
+        mh_tv_about_us = findViewById(R.id.mh_tv_about_us);
+    }
+
+    private void mh_tv_about_us() {
+        mh_tv_about_us.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MemberHome.this,"Clicked on About Us", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void mh_tv_contact_us() {
+        mh_tv_contact_us.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MemberHome.this,"Clicked on Contact Us", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void mh_tv_leaderboard() {
+        mh_tv_leaderboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MemberHome.this,"Clicked on Leaderboard", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void mh_tv_play() {
+        mh_tv_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MemberHome.this,"Clicked on Play", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void mh_tv_learn() {
+        mh_tv_learn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MemberHome.this,"Clicked on Learn", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
 }
