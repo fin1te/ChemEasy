501d568b16bb02e7073d6f132ff0ec34e21babac
fin1te <fin1te@fin1te.github.com>
2021-02-08 19:55:11 +0530

    Added LearnHome Activity made made a new button layout with a mix of LongButtons and SquareButtons

---
+++ app/src/main/java/com/finite/chemeasy/LearnHome.java
@@ -0,0 +1,14 @@
+package com.finite.chemeasy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LearnHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn_home);
+    }
+}
